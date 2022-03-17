2b2907a36c4ef44b5919f9926a5446765aa232b5
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-03-17 14:06:51 +0900

    실전편 01&02 다듬기

--- 01/up&down.js
+++ 01/up&down.js
@@ -1,14 +1,6 @@
 let count = 0;
 let randomNumber = Math.floor(Math.random() * 100 + 1);
 
-document.querySelector("try").onkeypress = function (e) {
-  // 눌린 키가 [enter]키라면
-  if (e.keycode == 13 || e.which == e) {
-    finding();
-    return false; // keypress 이벤트가 발생했을 때 브라우저가 기본으로 하는 동작 취소
-  }
-};
-
 function finding() {
   var userNumber = document.querySelector("#try").value;
   if (userNumber >= 1 && userNumber <= 100) {
