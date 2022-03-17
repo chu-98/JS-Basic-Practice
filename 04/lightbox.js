878789a8bd63fce5c081cfcd9c3cba98ce93c716
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-03-17 21:14:38 +0900

    실전편 04 끝

---
+++ 04/lightbox.js
@@ -0,0 +1,16 @@
+var pics = document.querySelectorAll(".pic");
+var lightbox = document.querySelector("#lightbox");
+var lightboxImage = document.querySelector("#lightboxImage");
+
+for (var i = 0; i < pics.length; i++) {
+  pics[i].addEventListener("click", showLightbox); // 이미지 누르면 showLight 함수 실행
+}
+
+function showLightbox() {
+  var bigLocation = this.getAttribute("data-src"); // 받은 객체의 "data-src" 속성을 가져와
+  lightboxImage.setAttribute("src", bigLocation);
+  lightbox.style.display = "block";
+}
+lightbox.onclick = function () {
+  lightbox.style.display = "none";
+};
